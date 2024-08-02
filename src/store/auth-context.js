899da2d3bc2993d4d29b32f80b9592fb63501836
Sumit Pandey <heerjnv@gmail.com>
2024-08-02 10:39:08 +0530

    5 Min Logout timer set

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,16 +10,29 @@ const AuthContext = React.createContext({
 export const AuthProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
+  const [timeoutId, setTimeoutId] = useState(null);
 
   const isUserLoggedIn = !!token;
 
   const loginHandler = (token) => {
     localStorage.setItem("token", token);
     setToken(token);
+    resetLogoutTimer();
   };
   const signupHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const startLogoutTimer = () => {
+    const id = setTimeout(() => {
+      signupHandler();
+    }, 5 * 60 * 1000);
+    setTimeoutId(id);
+  };
+  const resetLogoutTimer = () => {
+    clearTimeout(timeoutId);
+    startLogoutTimer();
   };
 
   const contextValue = {
